95b2bd5aedb7781757cb62a9b29ffb4ee333ebdb
Jordan Dale <jordan.dale2@gmail.com>
2019-01-23 10:33:01 -0500

    Exploring using Thunk via Redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { simpleAction } from "./actions/simpleAction";
+import { connect } from "react-redux";
+import logo from "./logo.svg";
+import "./App.css";
+
+const mapStateToProps = state => ({
+  ...state
+});
+
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+});
+
 
 class App extends Component {
+  simpleAction = event => {
+    this.props.simpleAction();
+  };
   render() {
     return (
       <div className="App">
@@ -19,10 +33,15 @@ class App extends Component {
           >
             Learn React
           </a>
+          <pre>{JSON.stringify(this.props)}</pre>
+          <button onClick={this.simpleAction}>Test redux action</button>
         </header>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
